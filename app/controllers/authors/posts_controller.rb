f2a2c3431a2f2fb119bac7dad3fb49fb6e79216a
Ian293382 <moshou5571228@gmail.com>
2022-05-31 06:06:05 +0800

    install redis

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -58,6 +58,18 @@ module Authors
       end
     end
 
+
+    def publish
+      post = Post.find(element.dataset[:post_id])
+      post.update(published: true, published_at: Time.now)
+    end
+
+    def unpublish
+      post = Post.find(element.dataset[:post_id])
+      post.update(published: false, published_at: nil)
+    end
+
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_post
